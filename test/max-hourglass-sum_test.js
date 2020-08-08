b942dd3ac5a31a25334dbd14170cb10c09959238
Becca9941 <rebeccawilliams9941@gmail.com>
2020-08-08 13:36:34 +1000

    :white_check_mark: Expect sum of all hourglass tops in row.

--- test/max-hourglass-sum_test.js
+++ test/max-hourglass-sum_test.js
@@ -2,11 +2,11 @@ const expect = require('chai').expect;
 const maxHourglassSum = require('../arrays/max-hourglass-sum');
 
 describe('maxHourglassSum should', function() {
-  it('find sum of largest hourglass.', function() {
+  it('sum all hourglass tops in a row', function() {
 
     const tests = [
-      { input: [[1,1,1]], result: 3 },
-      { input: [[0,1,0]], result: 1 },
+      { input: [[1,1,1]], result: [3] },
+      { input: [[0,1,0]], result: [1] },
     ];
 
     tests.forEach(function(test) {
