9820d2044ba22b4394343b5d360e4e0a506a0fac
Becca9941 <rebeccawilliams9941@gmail.com>
2020-08-08 10:15:06 +1000

    :sparkles: Reverse arrays.

--- test/reverse-array_test.js
+++ test/reverse-array_test.js
@@ -3,8 +3,9 @@ const reverseArray = require('../arrays/reverse-array');
 
 describe('reverseArray should', function() {
   it('flip elements so they appear in reverse order', function() {
-    // 1432
     expect(reverseArray([1])).to.eql([1]);
     expect(reverseArray([1,4])).to.eql([4,1]);
+    expect(reverseArray([1,4,3])).to.eql([3,4,1]);
+    expect(reverseArray([1,4,3,2])).to.eql([2,3,4,1]);
   });
 });
