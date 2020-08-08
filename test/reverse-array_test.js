f2a34e01f5bfa1320c1f68a14879000a2950e949
Becca9941 <rebeccawilliams9941@gmail.com>
2020-08-08 10:12:54 +1000

    :white_check_mark: Expect 1 4 to become 4 1.

--- test/reverse-array_test.js
+++ test/reverse-array_test.js
@@ -5,6 +5,6 @@ describe('reverseArray should', function() {
   it('flip elements so they appear in reverse order', function() {
     // 1432
     expect(reverseArray([1])).to.eql([1]);
-    //expect(reverseArray([1,4])).to.equal([4,1]);
+    expect(reverseArray([1,4])).to.eql([4,1]);
   });
 });
