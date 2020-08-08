2698992994b70d143e820408e207e439c6b682bc
Becca9941 <rebeccawilliams9941@gmail.com>
2020-08-08 14:43:30 +1000

    :recycle: Expect to find max hourglass sum.

--- test/max-hourglass-sum_test.js
+++ test/max-hourglass-sum_test.js
@@ -5,8 +5,10 @@ describe('maxHourglassSum should', function() {
   it('find sum of one hourglass', function() {
 
     const tests = [
-      { input: [[1,1,1],[0,1,0],[1,1,1]], result: 7},
-      { input: [[1,1,0],[1,0,0],[1,1,0]], result: 5},
+      { input: [[1,1,1],[0,1,0],[1,1,1]], result: 7 },
+      { input: [[1,1,0],[1,0,0],[1,1,0]], result: 4 },
+      { input: [[0,2,4,4],[0,0,2,0],[0,1,2,4]], result: 19 },
+      { input: [[1,1,1,0,0,0],[0,1,0,0,0,0],[1,1,1,0,0,0],[0,0,2,4,4,0],[0,0,0,2,0,0],[0,0,1,2,4,0]], result: 19 }
     ];
 
     tests.forEach(function(test) {
