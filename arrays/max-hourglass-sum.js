a924dcf540fc51ae346866806bfc98c71dd39521
Becca9941 <rebeccawilliams9941@gmail.com>
2020-08-08 14:43:49 +1000

    :construction: Find max hourglass sum.

--- arrays/max-hourglass-sum.js
+++ arrays/max-hourglass-sum.js
@@ -3,13 +3,13 @@ function maxHourglassSum(arr) {
   for(let row = 0; row < arr.length-2; row++) {
     for(let col = 0; col < arr[row].length-2; col++) {
       let top = arr[row][col] + arr[row][col+1] + arr[row][col+2];
-      let middle = arr[row][col+1];
+      let middle = arr[row+1][col+1];
       let bottom = arr[row+2][col] + arr[row+2][col+1] + arr[row+2][col+2];
 
       hourglassSums.push(sum([top + middle + bottom]));
     }
   }
-  return Math.max(hourglassSums);
+  return Math.max(...hourglassSums);
 }
 
 function sum(arr) {
