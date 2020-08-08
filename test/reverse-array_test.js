7e21ced6b9f9cd48b7698fe82555c9e2c8bae785
Becca9941 <rebeccawilliams9941@gmail.com>
2020-08-08 10:09:07 +1000

    :white_check_mark: Compare array wit array instead.

--- test/reverse-array_test.js
+++ test/reverse-array_test.js
@@ -4,6 +4,7 @@ const reverseArray = require('../arrays/reverse-array');
 describe('reverseArray should', function() {
   it('flip elements so they appear in reverse order', function() {
     // 1432
-    expect(reverseArray([1])).to.equal(1);
+    expect(reverseArray([1])).to.eql([1]);
+    //expect(reverseArray([1,4])).to.equal([4,1]);
   });
 });
