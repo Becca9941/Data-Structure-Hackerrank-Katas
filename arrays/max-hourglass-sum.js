9ec310dabf029f5c53e6bba27530d7df5064c541
Becca9941 <rebeccawilliams9941@gmail.com>
2020-08-08 13:50:48 +1000

    :recycle: Rename tops to caps, works for both top and bot.

--- arrays/max-hourglass-sum.js
+++ arrays/max-hourglass-sum.js
@@ -1,14 +1,14 @@
 function maxHourglassSum(arr) {
-  const hourglassTopSums = [];
+  const hourglassCapSums = [];
   const row = arr[0];
 
   for(let i = 0; i < row.length-2; i++) {
-    let hourglassTop = [row[i], row[i+1], row[i+2]];
-    hourglassTopSums.push(sum(hourglassTop));
+    let hourglassCap = [row[i], row[i+1], row[i+2]];
+    hourglassCapSums.push(sum(hourglassCap));
   }
 
 
-  return hourglassTopSums;
+  return hourglassCapSums;
 }
 
 function sum(arr) {
