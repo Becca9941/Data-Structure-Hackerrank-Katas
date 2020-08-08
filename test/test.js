f4b03670eaa972380770fb9e0fe2bb9ec31ef634
Becca9941 <rebeccawilliams9941@gmail.com>
2020-08-08 10:01:56 +1000

    :fire: Remove setup test.

--- test/test.js
+++
@@ -1,8 +0,0 @@
-const assert = require('chai').assert;
-const expect = require('chai').expect;
-
-describe('Setup test', function() {
-  it('should pass and fail as expected', function() {
-    expect(true).to.equal(true);
-  });
-});
