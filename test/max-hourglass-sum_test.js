be4a1df3fbc86107b3186da52e73cee9f7779968
Becca9941 <rebeccawilliams9941@gmail.com>
2020-08-08 21:50:31 +1000

    :construction: Improve readability of tests.

--- test/max-hourglass-sum_test.js
+++ test/max-hourglass-sum_test.js
@@ -2,13 +2,27 @@ const expect = require('chai').expect;
 const maxHourglassSum = require('../arrays/max-hourglass-sum');
 
 describe('maxHourglassSum should', function() {
-  it('find sum of one hourglass', function() {
+  it('find max hourglass sum', function() {
 
     const tests = [
-      { input: [[1,1,1],[0,1,0],[1,1,1]], result: 7 },
-      { input: [[1,1,0],[1,0,0],[1,1,0]], result: 4 },
-      { input: [[0,2,4,4],[0,0,2,0],[0,1,2,4]], result: 19 },
-      { input: [[1,1,1,0,0,0],[0,1,0,0,0,0],[1,1,1,0,0,0],[0,0,2,4,4,0],[0,0,0,2,0,0],[0,0,1,2,4,0]], result: 19 }
+      { input: [[1,1,1],
+	        [0,1,0],
+	        [1,1,1]],result: 7 },
+
+      { input: [[1,1,0],
+	        [1,0,0],
+	        [1,1,0]], result: 4 },
+
+      { input: [[0,2,4,4],
+	        [0,0,2,0],
+	        [0,1,2,4]], result: 19 },
+
+      { input: [[1,1,1,0,0,0],
+	        [0,1,0,0,0,0],
+	        [1,1,1,0,0,0],
+	        [0,0,2,4,4,0],
+	        [0,0,0,2,0,0],
+	        [0,0,1,2,4,0]], result: 19 }
     ];
 
     tests.forEach(function(test) {
