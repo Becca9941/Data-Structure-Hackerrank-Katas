60fe4552207ff3163ef8c7a7a8aaaadf34ac8a07
Becca9941 <rebeccawilliams9941@gmail.com>
2020-08-08 21:53:20 +1000

    :recycle: Change lets to consts for non-changing variables.

--- arrays/max-hourglass-sum.js
+++ arrays/max-hourglass-sum.js
@@ -1,10 +1,11 @@
 function maxHourglassSum(arr) {
-  let hourglassSums = [];
+  const hourglassSums = [];
+
   for(let row = 0; row < arr.length-2; row++) {
     for(let col = 0; col < arr[row].length-2; col++) {
-      let top = arr[row][col] + arr[row][col+1] + arr[row][col+2];
-      let middle = arr[row+1][col+1];
-      let bottom = arr[row+2][col] + arr[row+2][col+1] + arr[row+2][col+2];
+      const top = arr[row][col] + arr[row][col+1] + arr[row][col+2];
+      const middle = arr[row+1][col+1];
+      const bottom = arr[row+2][col] + arr[row+2][col+1] + arr[row+2][col+2];
 
       hourglassSums.push(top + middle + bottom);
     }
